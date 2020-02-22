74514ba9c2adce283a43f0c24cc03c3cc99cccaa
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-22 02:27:32 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,28 @@
+class Patient
+  @@all=[]
+  attr_accessor :name
+  
+  def initialize(name:)
+    @name=name
+    @@all << self 
+  end 
+  
+  def new_appointment(doctor:,date:)
+     Apointment.new(date,self,doctor)
+  end 
+  
+  def self.all 
+    @@all
+  end 
+  
+  def appointments 
+   Apointment.all.select{|appointment| appointment.patient==self}
+  end 
+  
+  
+  def doctors
+   appointments.map{|appointment| appointment.doctor}
+  end
+  
+    
+end 
