5dbb32fbb4c781feca6ad8de81aea90f98b0f097
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-22 03:18:05 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ class Appointment
   @@all=[]
   attr_accessor :doctor,:patient,:date
   
-  def initialize(date="",patient="",,doctor="")
+  def initialize(date="",patient="",doctor="")
     @date =date
     @patient=patient
     @doctor=doctor
