d5945ae2b2560f7ce17023e5d9d2d8ca69f9473d
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-02-22 03:16:47 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -13,8 +13,10 @@ class Doctor
   end 
   
   def appointments
-    Appointment.all.select{|appointment|  appointment.doctor ==self}
-  end 
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
   
   def new_appointment (patient,date)
     Appointment.new(self,patient,date)
